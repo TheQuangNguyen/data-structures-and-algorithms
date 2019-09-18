0edfdba575527df5c451fe1e15be6677aec0198c
Quang Nguyen <quangn2595@gmail.com>
2019-09-17 20:23:36 -0700

    complete code and test for stretch goal

--- code401challenges/src/main/java/code401challenges/ArrayShift.java
+++ code401challenges/src/main/java/code401challenges/ArrayShift.java
@@ -6,14 +6,13 @@ public class ArrayShift {
     // Without utilizing any of the built-in methods available to your language,
     // return an array with the new value added at the middle index.
 
-
     public static int[] insertShiftArray(int[] arr, int numberToAdd) {
         int[] shiftArray = new int[arr.length+1];
         int middleIndex = shiftArray.length/2;
         int pointer = 0;    // pointer to traverse through input array
 
-        // loop through the new array and insert values from the input array into new array unless the index is equals
-        // to
+        // loop through the new array and insert values from the input array into new array
+        // unless the index is equals to middleIndex
         for (int i = 0; i < shiftArray.length; i++) {
             if (i == middleIndex) {
                 shiftArray[i] = numberToAdd;
@@ -22,6 +21,30 @@ public class ArrayShift {
             }
         }
 
+        return shiftArray;
+    }
+
+    // write a second function that removes an element from the middle index
+    // and shifts other elements in the array to fill the new gap.
+
+    public static int[] removeShiftArray(int[] arr) {
+
+        // checks if the input array is empty
+        if (arr.length == 0) {
+            return new int[]{};
+        }
+
+        int[] shiftArray = new int[arr.length -1];
+        int middleIndex = arr.length / 2;
+        int pointer = 0;
+
+        for (int i = 0; i < arr.length; i++) {
+            if (i == middleIndex) {
+                continue;
+            } else {
+                shiftArray[pointer++] = arr[i];
+            }
+        }
         return shiftArray;
     }
 }
