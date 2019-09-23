20539f9e4dc1ab71662c4d8120c9f73ccc9d8c38
Quang Nguyen <quangn2595@gmail.com>
2019-09-23 07:04:31 -0700

    checkpoint created tests and code for implementation of linked list

---
+++ Data-Structures/src/main/java/Data/Structures/linkedList/Node.java
@@ -0,0 +1,10 @@
+package Data.Structures.linkedList;
+
+public class Node {
+    public int value;
+    public Node next = null;
+
+    public Node(int value) {
+        this.value = value;
+    }
+}
