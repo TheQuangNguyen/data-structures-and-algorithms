b2aa4d6260f58c548fa95aee10b8f473de7ab0cd
Quang Nguyen <quangn2595@gmail.com>
2019-10-22 21:35:55 -0700

    finished implementing merge sort in code and testing

--- Data-Structures/src/test/java/sorting/MergeSortTest.java
+++ Data-Structures/src/test/java/sorting/MergeSortTest.java
@@ -5,46 +5,46 @@ import org.junit.Test;
 
 import static org.junit.Assert.*;
 
-public class InsertionSortTest {
+public class MergeSortTest {
 
-    InsertionSort insertionSort;
+    MergeSort mergeSort;
     @Before
     public void setUp() throws Exception {
-        insertionSort = new InsertionSort();
+        mergeSort = new MergeSort();
     }
 
     // Test for empty array
     @Test
-    public void testInsertionSortEmptyArray() {
+    public void testMergeSortEmptyArray() {
         int[] arr = new int[0];
-        int[] sortedArr = insertionSort.sort(arr);
+        int[] sortedArr = mergeSort.sort(arr);
 
         assertArrayEquals(arr, sortedArr);
     }
 
     // Test for one number in array
     @Test
-    public void testInsertionSortArrayOneElement() {
+    public void testMergeSortArrayOneElement() {
         int[] arr = new int[]{7};
-        int[] sortedArr = insertionSort.sort(arr);
+        int[] sortedArr = mergeSort.sort(arr);
 
         assertArrayEquals(arr, sortedArr);
     }
 
     // Test for 2 numbers in array in order
     @Test
-    public void testInsertionSortArrayTwoElementsInOrder() {
+    public void testMergeSortArrayTwoElementsInOrder() {
         int[] arr = new int[]{3,7};
-        int[] sortedArr = insertionSort.sort(arr);
+        int[] sortedArr = mergeSort.sort(arr);
 
         assertArrayEquals(arr, sortedArr);
     }
 
     // Test for 2 numbers in array not in order
     @Test
-    public void testInsertionSortArrayTwoElementsNotInOrder() {
+    public void testMergeSortArrayTwoElementsNotInOrder() {
         int[] arr = {7,3};
-        int[] sortedArr = insertionSort.sort(arr);
+        int[] sortedArr = mergeSort.sort(arr);
         int[] expected = {3,7};
 
         assertArrayEquals(expected, sortedArr);
@@ -52,9 +52,9 @@ public class InsertionSortTest {
 
     // Test for multiple numbers in array not in order
     @Test
-    public void testInsertionSortArrayMultipleElementsNotInOrder() {
+    public void testMergeSortArrayMultipleElementsNotInOrder() {
         int[] arr = new int[]{7,8,5,3,6,1,9};
-        int[] sortedArr = insertionSort.sort(arr);
+        int[] sortedArr = mergeSort.sort(arr);
         int[] expected = new int[]{1,3,5,6,7,8,9};
 
         assertArrayEquals(expected, sortedArr);
@@ -62,18 +62,18 @@ public class InsertionSortTest {
 
     // Test for multiple numbers in array in order already
     @Test
-    public void testInsertionSortArrayMultipleElementsInOrder() {
+    public void testMergeSortArrayMultipleElementsInOrder() {
         int[] arr = new int[]{1,3,5,6,7,8,9};
-        int[] sortedArr = insertionSort.sort(arr);
+        int[] sortedArr = mergeSort.sort(arr);
 
         assertArrayEquals(arr, sortedArr);
     }
 
     // Test for negative numbers in array not in order
     @Test
-    public void testInsertionSortArrayMultipleElementsIncludingNegativeNotInOrder() {
+    public void testMergeSortArrayMultipleElementsIncludingNegativeNotInOrder() {
         int[] arr = new int[]{3,-9,-2,0,2,5,1};
-        int[] sortedArr = insertionSort.sort(arr);
+        int[] sortedArr = mergeSort.sort(arr);
         int[] expected = new int[]{-9,-2,0,1,2,3,5};
 
         assertArrayEquals(expected, sortedArr);
@@ -81,9 +81,9 @@ public class InsertionSortTest {
 
     // Test for duplicate numbers in array not in order
     @Test
-    public void testInsertionSortArrayDuplicateNumbersNotInOrder() {
+    public void testMergeSortArrayDuplicateNumbersNotInOrder() {
         int[] arr = new int[]{7,3,6,9,7,0,6,1};
-        int[] sortedArr = insertionSort.sort(arr);
+        int[] sortedArr = mergeSort.sort(arr);
         int[] expected = new int[]{0,1,3,6,6,7,7,9};
 
         assertArrayEquals(expected, sortedArr);
@@ -91,9 +91,9 @@ public class InsertionSortTest {
 
     // Test for array in reverse order
     @Test
-    public void testInsertionSortArrayReverseOrder() {
+    public void testMergeSortArrayReverseOrder() {
         int[] arr = new int[]{13,11,9,7,5,3,1};
-        int[] sortedArr = insertionSort.sort(arr);
+        int[] sortedArr = mergeSort.sort(arr);
         int[] expected = new int[]{1,3,5,7,9,11,13};
 
         assertArrayEquals(expected, sortedArr);
