5b261ffa2c11ff6c4d736db4dc4f3ced96a83057
Quang Nguyen <quangn2595@gmail.com>
2019-10-28 10:53:51 -0700

    got all the methods for hashtable coded. Now working on testing.

---
+++ Data-Structures/src/main/java/hashtable/Hashtable.java
@@ -0,0 +1,88 @@
+package hashtable;
+
+import java.util.Iterator;
+import java.util.LinkedList;
+import linkedList.Node;
+
+import java.util.NoSuchElementException;
+
+public class Hashtable<K, V> {
+
+    private LinkedList<Entry<K, V>>[] hashtable;
+    private static final double DEFAULT_LOAD_FACTOR = 0.75;
+    private static final int DEFAULT_INITIAL_CAPACITY = 10;
+    private static final int HASH_PRIME_NUMBER = 599;
+
+    // Define an entry in the hash table that holds the key and value pair
+    private class Entry<K, V> {
+        private K key;
+        private V value;
+
+        public Entry(K key, V value) {
+            this.key = key;
+            this.value = value;
+        }
+
+        public K getKey() {
+            return key;
+        }
+
+        public V getValue() {
+            return value;
+        }
+    }
+
+    // Constructor for hashtable that initialize the array of linked list to have an initial size
+    public Hashtable() {
+        this.hashtable = new LinkedList[DEFAULT_INITIAL_CAPACITY];
+    }
+
+    // turns the key into a hash code that will be used as the index to place the entry in the table
+    public int hash(K key) {
+        String keyAsString = key.toString();
+        int sumOfASCIIValues = 0;
+        for (int i = 0; i < keyAsString.length(); i++) {
+            sumOfASCIIValues += (int)keyAsString.charAt(i);
+        }
+        int tableSize = hashtable.length;
+        int hashCode = (sumOfASCIIValues * HASH_PRIME_NUMBER) % tableSize;
+        return hashCode;
+    }
+
+    // add a key value pair into the hashtable
+    public boolean add(K key, V value) {
+        if (contains(key)) {
+            return false;
+        }
+        Entry newEntry = new Entry(key, value);
+        int hashCode = hash(key);
+        hashtable[hashCode].add(newEntry);
+        return true;
+    }
+
+    // takes in a key and return the value from the table
+    public V get(K key) {
+        int hashCode = hash(key);
+        Iterator<Entry<K, V>> iterator = hashtable[hashCode].iterator();
+        while (iterator.hasNext()) {
+            Entry currentEntry = (Entry)iterator.next().getValue();
+            if (currentEntry.getKey().equals(key)) {
+                return (V)currentEntry.getValue();
+            }
+        }
+        throw new NoSuchElementException("This key does not exist in this hashtable");
+    }
+
+    // takes in a key and return a boolean, indicating if the key exists in the table already
+    public boolean contains(K key) {
+        int hashCode = hash(key);
+        Iterator<Entry<K, V>> iterator = hashtable[hashCode].iterator();
+        while (iterator.hasNext()) {
+            Entry currentEntry = (Entry) iterator.next().getValue();
+            if (currentEntry.getKey().equals(key)) {
+                return true;
+            }
+        }
+        return false;
+    }
+}
---
+++ Data-Structures/src/test/java/hashtable/HashtableTest.java
@@ -0,0 +1,20 @@
+package hashtable;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class HashtableTest {
+
+    Hashtable<Integer, String> hashtable;
+
+    @Before
+    public void setUp() throws Exception {
+        hashtable = new Hashtable<>();
+    }
+
+    //
+    @Test
+    public
+}
